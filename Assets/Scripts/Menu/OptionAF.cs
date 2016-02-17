708a0a9ec5208d2b8d6069964055d9dc81f4bbee
DasArmin <armin.hunter@hotmail.com>
2016-02-17 11:18:44 +0100

    Added Timer, Score and Game options

---
+++ Assets/Scripts/Menu/OptionAF.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionAF : MonoBehaviour
+{ 
+    [SerializeField] Dropdown _dropdownAF;
+    
+	void Update ()
+    {
+	    if (_dropdownAF.value == 0)
+        {
+            QualitySettings.anisotropicFiltering = AnisotropicFiltering.Enable;
+        }
+
+        else if (_dropdownAF.value == 1)
+        {
+            QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
+        }
+
+        else
+        {
+            QualitySettings.anisotropicFiltering = AnisotropicFiltering.ForceEnable;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ScoreCount.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreCount : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+    private float scoreCounter;
+    public float ScoreCounter
+    {
+        get { return scoreCounter; }
+        set { scoreCounter = value; }
+    }
+
+    void Update()
+    {
+        scoreText.text = "Score: " + scoreCounter;
+    }
+}
