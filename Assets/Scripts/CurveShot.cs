8fade62251bce106926b445285a47a53574341e5
Pim Horeman <pimhoreman@gmail.com>
2016-02-18 17:15:20 +0100

    CurveShot Script, Updated
    The Catapult can now shoot more then once every 6 sec.

--- Assets/Scripts/CurveShot.cs
+++ Assets/Scripts/CurveShot.cs
@@ -13,25 +13,41 @@ public class CurveShot : MonoBehaviour
 
 	void Awake()
 	{
+		//print ("Awake Activates");
 		myCatapult = transform; 
-		StartCoroutine(SimulateProjectile());
+
+		//With the invokeReapeat whe can shoot more then 1 projectile.
+		InvokeRepeating("CoroutineProjectile",1,5);
+		//print ("InvokeRepeating ON");
+
 	}
 	
+	void CoroutineProjectile()
+	{
+		//print ("StartCoroutine ON");
+		//Starts the SimulateProjectile IEnumarator.
+		StartCoroutine (SimulateProjectile());
+	
+	}
+
 	IEnumerator SimulateProjectile()
 	{
-		// Short delay added before Projectile is thrown
+		//print ("IEnumerator ON");
+		
+		
+		// Short delay added before Projectile is thrown.
 		yield return new WaitForSeconds(delayTime);
 		
 		// Move projectile to the position of throwing object + add some offset if needed.
-		projectile.position = myCatapult.position + new Vector3(0, 0.0f, 0);
+		projectile.position = myCatapult.position + new Vector3(0, 1f, 0);
 		
-		// Calculate distance to target
+		// Calculate distance to target.
 		float targetDistance = Vector3.Distance(projectile.position, target.position);
 		
 		// Calculate the velocity needed to throw the object to the target at specified angle.
 		float projectileVelocity = targetDistance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / gravity);
 		
-		// Extract the X  Y componenent of the velocity
+		// Extract the X  Y componenent of the velocity.
 		float VelocityX = Mathf.Sqrt(projectileVelocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad);
 		float VelocityY = Mathf.Sqrt(projectileVelocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad);
 		
@@ -45,12 +61,20 @@ public class CurveShot : MonoBehaviour
 		
 		while (elapseTime < flightDuration)
 		{
+			//The Projectile will keep flying until it has reached his target location.
 			projectile.Translate(0, (VelocityY - (gravity * elapseTime)) * Time.deltaTime, VelocityX * Time.deltaTime);
 			
 			elapseTime += Time.deltaTime;
-
+			
+			//print ("Projectile Is Flying!");
+			
 			yield return null;
 		}
-	} 
+		
+		print ("Projectile Landed");
+		
+		//Instantiate (projectile);	
+	}
+
 
 }
---
+++ Assets/Scripts/PlayStuff.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayStuff : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider col)
+	{
+			Destroy (this.gameObject);
+			print ("Dat voelt lekker");
+	}
+}
