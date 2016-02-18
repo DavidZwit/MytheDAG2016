6ae98746d5f6661a62439275d7f46596773bce22
DavidZwit <davidzwitser@gmail.com>
2016-02-18 22:38:39 +0100

    Adding the scene with everyones part in it (not done)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Vector3 movementVector;
+    private CharacterController characterController;
+
+    public Animator anim;
+
+    private float speed;
+    private float movementSpeed = 10;
+
+    private bool Grounded = false;
+    private float minSensitivity = 0.5f;
+
+    private float gravity = 40;
+
+    private Quaternion playerRotation;
+    private float turnInput;
+    private float rotateSpeed = 50f;
+
+    // Use this for initialization
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        speed = movementSpeed;
+        playerRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Rotation();        
+        Movement();         
+    }
+
+    void Movement()
+    {
+        if (characterController.isGrounded)
+        {
+            Vector3 keyInputs = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            movementVector = new Vector3(keyInputs.x,0,keyInputs.z);
+            movementVector = transform.TransformDirection(movementVector);
+            movementVector *= speed;
+            if (Input.GetAxis("Horizontal") > minSensitivity || Input.GetAxis("Horizontal") < -minSensitivity)
+            {
+                anim.SetBool("Walk", true);
+            }
+            else
+            {
+                anim.SetBool("Walk", false);
+            }
+            
+            
+
+        }  
+        
+
+        movementVector.y -= gravity * Time.deltaTime;
+        characterController.Move(movementVector * Time.deltaTime);
+    }
+
+    void Rotation()
+    { 
+        float inputs = Input.GetAxis("Mouse X");
+        if (inputs != 0)
+        {
+            transform.eulerAngles += new Vector3(0, inputs * minSensitivity, 0);
+        }        
+
+    }
+}
+
+        
+
+
---
+++ Assets/Scripts/UI/AddScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddScore : MonoBehaviour
+{
+    private ScoreCount scoreCount;
+
+    void Start()
+    {
+        scoreCount = GameObject.Find("Score").GetComponent<ScoreCount>();
+    }
+
+    public void IncreaseScore()
+    {
+        scoreCount._ScoreCounter += 100;
+    }
+}
