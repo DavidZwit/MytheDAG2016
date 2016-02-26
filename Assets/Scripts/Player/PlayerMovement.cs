b2a243a6e81cd797bb974fe65d48a5c83fcca1c1
koedijk <17798@ma-web.nl>
2016-02-26 16:05:42 +0100

    PlayerMovement Fix
    Fix

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,9 +4,10 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController characterController;
+
     private Vector3 movementVector;
-    public Animator anim;
+    private Rigidbody playerRigidbody;
+    public Animator _anim;
 
     private float speed;
     private float movementSpeed = 10f;
@@ -19,60 +20,88 @@ public class PlayerMovement : MonoBehaviour
     private Quaternion playerRotation;
     private float turnInput;
     [SerializeField]
-    private float rotateSpeed = 2f;
-
+    private float rotateSpeed = 10f;
+    private float Horizontal;
+    private float Vertical;
     // Use this for initialization
     void Start()
     {
-        characterController = gameObject.GetComponent<CharacterController>();
-
         speed = movementSpeed;
         playerRotation = transform.rotation;
-       // anim.SetBool("Idle", true);
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
-    {               
+    {        
+
         Movement();
-        MouseClick();       
+        MouseClick();
+        Animator();
     }
 
     void Movement()
     {
-        if (characterController.isGrounded)
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+        // Set the movement vector based on the axis input.
+        movementVector.Set(0f, 0f, Vertical);
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movementVector = movementVector.normalized * speed * Time.deltaTime;    
+        transform.Translate(movementVector);
+        // Move the player to it's current position plus the movement.
+        //playerRigidbody.MovePosition(transform.position + movementVector);
+        //playerRigidbody.MoveRotation(transform.rotation);
+        if (Horizontal > minSensitivity || Horizontal < -minSensitivity)
         {
-            Vector3 keyInputs = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            movementVector = new Vector3(0,0,keyInputs.z);
-            movementVector = transform.TransformDirection(movementVector);
-            movementVector *= speed;
-            
-            if (Input.GetAxis("Vertical") > minSensitivity || Input.GetAxis("Vertical") < -minSensitivity)
-            {
-               anim.SetBool("Run", true);                
-               anim.SetBool("Idle", false);
-            }
-            else
-            {
-               anim.SetBool("Run", false);             
-            }
-            if (keyInputs.x > minSensitivity || keyInputs.x <-minSensitivity)
-            {
-                transform.eulerAngles += new Vector3(0, keyInputs.x * rotateSpeed, 0);
-            }             
-
-        }  
-        
-
-        movementVector.y -= gravity * Time.deltaTime;
-        characterController.Move(movementVector * Time.deltaTime);
+            transform.eulerAngles += new Vector3(0, Horizontal * rotateSpeed, 0);
+        }
     }
+
+    void Animator()
+    {   bool walking;
+        if (walking = Horizontal != 0f || Vertical != 0f)
+        {
+            _anim.SetBool("Run", true);
+        }
+        else
+        {
+            _anim.SetBool("Idle", true);
+        }
+
+    }
+
+
+   /* void Movement()
+    {
+        
+        movementVector = new Vector3(0, 0, keyInputs.z);
+        movementVector = transform.TransformDirection(movementVector);
+        movementVector *= speed;
+
+        if (Input.GetAxis("Vertical") > minSensitivity || Input.GetAxis("Vertical") < -minSensitivity)
+        {
+            anim.SetBool("Run", true);
+            anim.SetBool("Idle", false);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+        if (keyInputs.x > minSensitivity || keyInputs.x < -minSensitivity)
+        {
+            transform.eulerAngles += new Vector3(0, keyInputs.x * rotateSpeed, 0);
+        }
+
+
+
+    }*/
 
     void MouseClick()
     {
         if (Input.GetMouseButton(0))
         {
-           anim.SetTrigger("Smash");
+            _anim.SetTrigger("Smash");
         }
     }
 }
