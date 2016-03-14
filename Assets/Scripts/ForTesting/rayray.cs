31a437857d43620319801ed57baa7b519c4021c3
SjoerdJakobs <18284@ma-web.nl>
2016-03-14 10:11:07 +0100

    fireBreath tests
    fireBreath tests

---
+++ Assets/Scripts/ForTesting/rayray.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rayray : MonoBehaviour {
+
+    bool checkhit;
+
+    public Transform cube;
+
+	void Update () {
+
+        checkhit = false;
+
+        RaycastHit hit = new RaycastHit();
+
+        checkhit = Physics.SphereCast(transform.position, 1, transform.forward, out hit);
+
+        if(checkhit)
+        {
+            cube.position = hit.point;
+            cube.rotation = Quaternion.LookRotation(hit.normal);
+        }
+	    
+	}
+}
