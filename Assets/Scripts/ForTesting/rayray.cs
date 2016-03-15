5606ba79a2e6894a2a99c013480cf1bb327552e4
SjoerdJakobs <18284@ma-web.nl>
2016-03-15 11:37:54 +0100

    fire sync
    fire sync

--- Assets/Scripts/ForTesting/rayray.cs
+++ Assets/Scripts/ForTesting/rayray.cs
@@ -3,20 +3,41 @@ using System.Collections;
 
 public class rayray : MonoBehaviour {
 
-    bool checkhit;
+    private bool checkhit;
 
-    public Transform cube;
+    [SerializeField]
+    private float maxFireRange;
+    private float fireRange;
+
+    [SerializeField]
+    private Transform cube;
 
 	void Update () {
+        
+        if(Input.GetButton("Fire1"))
+        {
+            fireRange += 0.1f;
+            if(fireRange > maxFireRange)
+            {
+                fireRange = maxFireRange;
+            }
+            print(fireRange);
+        }
+        else
+        {
+            fireRange = 0;
+            cube.position = transform.position;
+        }
 
         checkhit = false;
 
         RaycastHit hit = new RaycastHit();
 
-        checkhit = Physics.SphereCast(transform.position, 1, transform.forward, out hit);
+        checkhit = Physics.SphereCast(transform.position, 1, transform.forward, out hit, fireRange);
 
         if(checkhit)
         {
+            print(hit);
             cube.position = hit.point;
             cube.rotation = Quaternion.LookRotation(hit.normal);
         }
