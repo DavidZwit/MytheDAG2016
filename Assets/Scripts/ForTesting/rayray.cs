552fcaf8f9fd9b8fa9c88ee6f30c541a4f0f7a88
SjoerdJakobs <18284@ma-web.nl>
2016-03-20 02:35:31 +0100

    your breath smels like burning!
    your breath smels like burning!

--- Assets/Scripts/ForTesting/rayray.cs
+++ Assets/Scripts/ForTesting/rayray.cs
@@ -2,9 +2,7 @@
 using System.Collections;
 
 public class rayray : MonoBehaviour {
-
-    private bool checkhit;
-
+    
     [SerializeField]
     private float maxFireRange;
     private float fireRange;
@@ -13,35 +11,36 @@ public class rayray : MonoBehaviour {
     private Transform cube;
 
 	void FixedUpdate () {
-        
         if(Input.GetButton("Fire1"))
         {
-            fireRange += 0.1f;
+            fireRange += 0.13f;
             if(fireRange > maxFireRange)
             {
                 fireRange = maxFireRange;
             }
-            //print(fireRange);
         }
         else
         {
             fireRange = 0;
             cube.position = transform.position;
-            //print("testing");
         }
-
-        checkhit = false;
 
         RaycastHit hit = new RaycastHit();
-
-        checkhit = Physics.SphereCast(transform.position, 1, transform.forward, out hit, fireRange);
-
-        if(checkhit)
+        if(Physics.SphereCast(transform.position, 1, transform.forward, out hit, fireRange))
         {
-            //print(hit);
-            cube.position = hit.point;
-            cube.rotation = Quaternion.LookRotation(hit.normal);
+            OnHitObject(hit);
         }
-	    
 	}
+    void OnHitObject(RaycastHit hit)//this wil take in information abbout the object hit
+    {
+        print("test m8");
+        IDamageable damageableObject = hit.collider.GetComponent<IDamageable>();//check for component idamagable on the hit object
+        if (damageableObject != null)//"if object has idamagable"
+        {
+            damageableObject.TakeDamg(1);//damage it
+        }
+        cube.position = hit.point;
+        cube.rotation = Quaternion.LookRotation(hit.normal);
+        //Debug.Log(hit.collider.gameObject.name);
+    }
 }
