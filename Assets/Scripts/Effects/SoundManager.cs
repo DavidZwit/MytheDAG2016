3b0cf104a3bf6edbe8c57b5f2dc760198fe19f45
SjoerdJakobs <18284@ma-web.nl>
2016-03-25 01:24:30 +0100

    1 sound manager weg gehaald sinds er 2 waren
    1 sound manager weg gehaald sinds er 2 waren

--- Assets/Scripts/Effects/SoundManager.cs
+++ Assets/Scripts/Effects/SoundManager.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -15,4 +15,4 @@ public class SoundManager : MonoBehaviour
 
         source.PlayOneShot(myList[audioID], audioVol);
     }
-}
+}*/
