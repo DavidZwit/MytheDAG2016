f64d324ceef17957ef4d54b07f6bf7436a4019e8
DasArmin <armin.hunter@hotmail.com>
2016-03-30 17:12:24 +0200

    Removed Shadow Quality

--- Assets/Scripts/Menu/SavePlayerSettings.cs
+++ Assets/Scripts/Menu/SavePlayerSettings.cs
@@ -3,15 +3,12 @@ using System.Collections;
 
 public class SavePlayerSettings : MonoBehaviour
 {
-    private OptionShadowRes optionShadowRes;
     private OptionShadowDist optionShadowDist;
     private OptionTextureRes optionTextureRes;
     private OptionvSync optionvSync;
     private OptionAF optionAF;
     private OptionAA optionAA;
     
-    private int shadowResValue;
-    public int ShadowResValue { get { return shadowResValue; } }
     private int shadowDistValue;
     public int ShadowDistValue { get { return shadowDistValue; } }
     private int textureResValue;
@@ -25,7 +22,6 @@ public class SavePlayerSettings : MonoBehaviour
 
     void Awake()
     {
-        optionShadowRes = GameObject.Find("DropdownShadowRes").GetComponent<OptionShadowRes>();
         optionShadowDist = GameObject.Find("DropdownShadowDist").GetComponent<OptionShadowDist>();
         optionTextureRes = GameObject.Find("DropdownTextureRes").GetComponent<OptionTextureRes>();
         optionvSync = GameObject.Find("DropdownvSync").GetComponent<OptionvSync>();
@@ -37,13 +33,11 @@ public class SavePlayerSettings : MonoBehaviour
 
     public void SaveSettings()
     {
-        shadowResValue = optionShadowRes._shadowResValue;
         shadowDistValue = optionShadowDist._shadowDistValue;
         textureResValue = optionTextureRes._textureResValue;
         vsyncValue = optionvSync._vSyncValue;
         afValue = optionAF._afValue;
         aaValue = optionAA._aaValue;
-        PlayerPrefs.SetInt("ShadowResValue", shadowResValue);
         PlayerPrefs.SetInt("ShadowDistValue", shadowDistValue);
         PlayerPrefs.SetInt("TextureResValue", textureResValue);
         PlayerPrefs.SetInt("DropdownvSync", vsyncValue);
@@ -53,13 +47,11 @@ public class SavePlayerSettings : MonoBehaviour
 
     public void LoadSettings()
     {
-        shadowResValue = PlayerPrefs.GetInt("ShadowResValue");
         shadowDistValue = PlayerPrefs.GetInt("ShadowDistValue");
         textureResValue = PlayerPrefs.GetInt("TextureResValue");
         vsyncValue = PlayerPrefs.GetInt("DropdownvSync");
         afValue = PlayerPrefs.GetInt("AFValue");
         aaValue = PlayerPrefs.GetInt("AAValue");
-        optionShadowRes._shadowResValue = shadowResValue;
         optionShadowDist._shadowDistValue = shadowDistValue;
         optionTextureRes._textureResValue = textureResValue;
         optionvSync._vSyncValue = vsyncValue;
