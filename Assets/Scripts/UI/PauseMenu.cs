c51dc5462b5a4ba95efba5295f4a0a4e4dc76e10
Pim Horeman <pimhoreman@gmail.com>
2016-04-06 13:53:32 +0200

    Added All Sorts of Stuff
    Added Pimmez Branch To the Master
    Includes:
    -Pause Menu
    -Trebuchet
    -Health and Rampage bar
    -Minor Codes Changes

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour 
+{
+	[SerializeField] private GameObject resume;
+	[SerializeField] private GameObject restart;
+	[SerializeField] private GameObject mainmenu;
+	[SerializeField] private GameObject menuLayout;
+	[SerializeField] private GameObject cam;
+	[SerializeField] private GameObject hitFXPanel;
+
+	MouseOrbit mouseOr;
+
+	private bool isPaused = false;
+
+	private GameObject pausedMenu;
+
+	void Start()
+	{
+		mouseOr = cam.GetComponent<MouseOrbit>();
+		mouseOr.enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		ThePausedMenu ();
+	}
+
+	public void ThePausedMenu()
+	{
+		if (isPaused == true)
+		{
+			menuLayout.SetActive (true);
+			Cursor.visible = true;
+			Time.timeScale = 0;
+			mouseOr.enabled = false;
+			hitFXPanel.SetActive (false);
+		} 
+		else if(!isPaused)
+		{
+			menuLayout.SetActive (false);
+		}
+
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			isPaused = !isPaused;
+			Time.timeScale = 1;
+			Cursor.visible = false;
+			mouseOr.enabled = true;
+			hitFXPanel.SetActive (true);
+		}
+	}
+
+
+	public void BackToMenu()
+	{
+		SceneManager.LoadScene ("StartMenu");
+	}
+
+	public void Restart()
+	{
+		SceneManager.LoadScene ("MainScene");
+		//Application.runInBackground = true;
+		Time.timeScale = 1;
+		isPaused = false;
+	}
+
+	public void Resume()
+	{
+		isPaused = false;
+		Cursor.visible = false;
+		mouseOr.enabled = true;
+		hitFXPanel.SetActive (true);
+		Time.timeScale = 1;
+	}
+}
